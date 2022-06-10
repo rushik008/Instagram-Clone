dc8f87bbaff5a4c6c67c6af967bfebb33a0684d3
Rushik R Rathod <rathodrushikr@gmail.com>
2022-06-10 11:54:26 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to Instagram Clone</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
