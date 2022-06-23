aee43cf6ee616d8087d0e07f7d02fbd4f15dc915
Rushik R Rathod <rathodrushikr@gmail.com>
2022-06-23 14:47:47 +0530

    Search screen images edited

--- src/data/search.js
+++ src/data/search.js
@@ -1,33 +1,66 @@
 const staticImage1 =
-  'https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg';
-const staticImage2 =
-  'https://images.theconversation.com/files/38926/original/5cwx89t4-1389586191.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop';
-const staticImage3 =
-  'https://www.kindpng.com/picc/m/330-3309498_momos-momo-yaoming-memes-meme-laughing-man-meme.png';
-const staticImage4 =
   'https://cdn.shopify.com/s/files/1/0062/8083/9232/products/iron-man-mark7_620x.jpg?v=1637227128';
-const staticImage5 =
-  'https://imageio.forbes.com/blogs-images/jvchamary/files/2016/03/man_of_steel-1200x800.jpg?format=jpg&width=960';
-const staticImage6 =
+const staticImage2 =
+  'https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=';
+
+const staticImage3 =
   'https://blog.playstation.com/tachyon/2022/06/0c3c20a8d8514501524a0859461f391572ea6e61.jpg';
+const staticImage4 =
+  'https://i.guim.co.uk/img/media/67d0eadeb4cdce6d2ddacdf4faa8f3c3b630a811/0_80_3000_1802/master/3000.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f39071145c8494d76603abe505646353';
+const staticImage5 =
+  'https://static.parade.com/wp-content/uploads/2020/03/avengers-marvel.jpg';
+const staticImage6 =
+  'https://imageio.forbes.com/blogs-images/jvchamary/files/2016/03/man_of_steel-1200x800.jpg?format=jpg&width=960';
+const staticImage7 =
+  'https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111-1500x1000.jpg';
+const staticImage8 =
+  'https://cdn-prod.medicalnewstoday.com/content/images/articles/325/325466/man-walking-dog.jpg';
+const staticImage9 =
+  'https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg';
 
 export default [
   {discoveryImage: staticImage1},
+  {discoveryImage: staticImage2},
+  {discoveryImage: staticImage3},
+  {discoveryImage: staticImage4},
+  {discoveryImage: staticImage5},
+  {discoveryImage: staticImage6},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage8},
+  {discoveryImage: staticImage9},
   {discoveryImage: staticImage1},
+  {discoveryImage: staticImage2},
+  {discoveryImage: staticImage3},
+  {discoveryImage: staticImage4},
+  {discoveryImage: staticImage5},
+  {discoveryImage: staticImage6},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage8},
+  {discoveryImage: staticImage9},
   {discoveryImage: staticImage1},
   {discoveryImage: staticImage2},
+  {discoveryImage: staticImage3},
+  {discoveryImage: staticImage4},
+  {discoveryImage: staticImage5},
+  {discoveryImage: staticImage6},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage8},
+  {discoveryImage: staticImage9},
+  {discoveryImage: staticImage1},
   {discoveryImage: staticImage2},
-  {discoveryImage: staticImage2},
-  {discoveryImage: staticImage3},
-  {discoveryImage: staticImage3},
   {discoveryImage: staticImage3},
   {discoveryImage: staticImage4},
-  {discoveryImage: staticImage4},
-  {discoveryImage: staticImage4},
-  {discoveryImage: staticImage5},
-  {discoveryImage: staticImage5},
   {discoveryImage: staticImage5},
   {discoveryImage: staticImage6},
-  {discoveryImage: staticImage6},
-  {discoveryImage: staticImage6},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage7},
+  {discoveryImage: staticImage8},
+  {discoveryImage: staticImage9},
 ];
